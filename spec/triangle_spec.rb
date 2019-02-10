acc6493e787e958cdc4b44688dd149052e94deeb
uehara-delta <uehara.delta@gmail.com>
2019-02-10 16:10:35 +0900

    辺の長さが 0 やマイナスの場合のテストケースを追加

--- spec/triangle_spec.rb
+++ spec/triangle_spec.rb
@@ -9,10 +9,12 @@ describe Triangle do
 
   # 追加テストケース
   specify("順序は問わない") { expect(Triangle.guess(2, 1, 2)).to eq "二等辺三角形ですね！" }
-
   specify("直角三角形") do
     expect(Triangle.guess(3, 4, 5)).to eq "直角三角形ですね！"
     expect(Triangle.guess(3, 5, 4)).to eq "直角三角形ですね！"
   end
   # 辺の長さは整数なので直角二等辺三角形は判定不要
+
+  specify("長さが0") { expect(Triangle.guess(0, 3, 4)).to eq "三角形じゃないです＞＜" }
+  specify("負の数") { expect(Triangle.guess(-1, 3, 4)).to eq "三角形じゃないです＞＜" }
 end
